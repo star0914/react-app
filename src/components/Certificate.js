004dd0b6aa0bb2e7aca2d98ffb36aa1510ab014e
star0914 <semon.psi@outlook.com>
2020-07-20 19:29:40 +0300

    initial app

--- src/components/Certificate.js
+++ src/components/Certificate.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 import "../styling/Tabs.scss";
+import "../styling/Certificate.scss";
 
 //import JobDesc from "../components/json/JobDesc";
 
 function Certificate (props) {
   return <div>
-    <div>
+    <div className="certificate">
       <h2>Microsoft Technology Associate</h2>
       <h4>Software Development Fundamentals</h4>
       <h6>Received May 2019</h6>
       <ul>
-        <li>y</li>// TODO: 
+        <li>y</li>// TODO:
       </ul>
     </div>
-    <div>
+    <div className="certificate">
       <h2>Sheridan College</h2>
       <h4>Software Development Advanced Diploma</h4>
       <h6>Graduated December 2016</h6>
